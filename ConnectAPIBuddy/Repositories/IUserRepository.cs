31a4aee582ff7b0faef2e3cdaa94152dc5861bbd
KushanH <kushan989@gmail.com>
2019-10-22 14:37:53 +0530

    User repositoy added

---
+++ ConnectAPIBuddy/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ConnectAPIBuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectAPIBuddy.Repositories
+{
+    public interface IUserRepository
+    {
+        //GET
+        Task<User> GetUserByEmail(string email);
+        Task<bool> UserExists(string email);
+        Task<bool> UserAthorized(User user);
+
+        //POST
+        Task<bool> AddUser(User userIn);
+
+    }
+}
---
+++ ConnectAPIBuddy/Repositories/UserRepository.cs
@@ -0,0 +1,94 @@
+﻿using ConnectAPIBuddy.Models;
+using ConnectAPIBuddy.Services;
+using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectAPIBuddy.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IUserContext _context = null;
+        public UserRepository(IUserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            try
+            {
+                return await _context.UserLogin.FindSync<User>(user => user.Email.Equals(email)).FirstOrDefaultAsync();
+            }catch (Exception ex)
+            {
+                throw ex;
+            }
+        } 
+
+        public async Task<bool> UserExists(string email)
+        {
+            try
+            {
+                User user = await _context.UserLogin.FindSync<User>(u => u.Email.Equals(email)).FirstOrDefaultAsync();
+                if (user != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+
+        }
+
+        public async Task<bool> UserAthorized(User userIn)
+        {
+            try
+            {
+                User user = await _context.UserLogin.FindSync<User>(u => u.Email.Equals(userIn.Email)).FirstOrDefaultAsync();
+                if (user == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (user.Password.Equals(userIn.Password))
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+        public async Task<bool> AddUser(User userIn)
+        {
+            try
+            {
+                await _context.UserLogin.InsertOneAsync(userIn);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            
+        }
+    }
+}
