47b6b84263d1717f16d36804b0084243a4c5ca50
KushanH <kushan989@gmail.com>
2019-11-19 12:18:50 +0530

    substitude new models instead of older ones

--- ConnectAPIBuddy/Controllers/TestTriggerController.cs
+++ ConnectAPIBuddy/Controllers/TestTriggerController.cs
@@ -14,142 +14,183 @@ namespace ConnectAPIBuddy.Controllers
     [ApiController]
     public class TestTriggerController : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _httpClient;
 
-        public TestTriggerController(IHttpClientFactory httpClientFactory)
+        public TestTriggerController()
         {
-            _httpClientFactory = httpClientFactory;
+            _httpClient = new HttpClient();
         }
 
         [HttpPost]
-        public async Task<Result> TestRequest(Request req)
+        public async Task<DeliveryResponse> TestRequest(DeliveryRequest req)
         {
-            Result result = new Result();
-            using (var client = new HttpClient())
+            DeliveryResponse result = new DeliveryResponse();
+            if (req == null)
             {
-                client.DefaultRequestHeaders.Accept.Clear();
-                req.PayloadHeaders.ToList().ForEach(h =>
-                {
-                    if (h.Header == "Content-Type")
-                    {
-                        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(h.Value));
-                    }
-                    else
-                    {
-                        client.DefaultRequestHeaders.Add(h.Header, h.Value);
-                    }
-                });
+                return result;
+            }
+            
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            if (req.TestSettings != null) 
+            {
+                _httpClient.Timeout = TimeSpan.FromMilliseconds(req.TestSettings.TimeOutMs);
+            }
+            
 
-                if (req.Method.Equals("GET"))
+            req.PayloadHeaders.ToList().ForEach(h =>
+            {
+                if (h.Header == "Content-Type")
                 {
-                    try
-                    {
-                        var response = client.GetAsync(req.Url).Result;
-                        result.Body = response.Content.ReadAsStringAsync().Result;
-                        result.Status = response.StatusCode;
-                        if (response.IsSuccessStatusCode == false)
-                        {
-                            result.Body = response.ReasonPhrase;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Request timeout \nCannot Connect to the server \nTry Again!!!", "Connect API Buddy");
-                    }
+                    _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(h.Value));
                 }
-                else if (req.Method.Equals("POST"))
+                else
                 {
-                    if (req.BodyTabSelectedIndex == 0)
-                    {
-                        var hContent = new StringContent(req.PayloadBody);
-                        hContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                        try
-                        {
-                            var response = client.PostAsync(req.Url, hContent).Result;
-                            result.Body = response.Content.ReadAsStringAsync().Result;
-                            result.Status = response.StatusCode;
-                            if (response.IsSuccessStatusCode == false)
-                            {
-                                result.Body = response.ReasonPhrase;
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Request timeout \nCannot Connect to the server \nTry Again!!!", "Connect API Buddy");
-                        }
-                    }
-                    else if (req.BodyTabSelectedIndex == 1)
-                    {
-                        var form = new MultipartFormDataContent();
-                        //req.FormContent.ToList().ForEach(s =>
-                        //{
-                        //    form.Add()
-                        //});
-
-                        try
-                        {
-                            var Fileresponse = client.PostAsync(req.Url, form).Result;
-                            result.Body = Fileresponse.Content.ReadAsStringAsync().Result;
-                            result.Status = Fileresponse.StatusCode;
-                            if (Fileresponse.IsSuccessStatusCode == false)
-                            {
-                                result.Body = Fileresponse.ReasonPhrase;
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Request timeout \nCannot Connect to the server \nTry Again!!!", "Connect API Buddy");
-                        }
-
-                    }
+                    _httpClient.DefaultRequestHeaders.Add(h.Header, h.Value);
                 }
-                else if (req.Method.Equals("UPDATE"))
+            });
+
+            if (req.Method.Equals("GET"))
+            {
+                try
+                {
+                    var response = await _httpClient.GetAsync(req.Url);
+
+                    result.IsSuccess = response.IsSuccessStatusCode;
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        result.StatusText = response.ReasonPhrase;
+                    }else
+                    {
+                        result.Content = await response.Content.ReadAsStringAsync();
+                    }
+                    result.Status = response.StatusCode;
+                    result.RequestMessage = response.RequestMessage.ToString();
+                }
+                catch (Exception ex)
+                {
+                    result.StatusText = ex.Message;
+                    return result;
+                }
+            }
+            else if (req.Method.Equals("POST"))
+            {
+                if (req.BodyTabSelectedIndex == 0)
                 {
                     var hContent = new StringContent(req.PayloadBody);
                     hContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
                     try
                     {
-                        var response = client.PutAsync(req.Url, hContent).Result;
-                        result.Body = response.Content.ReadAsStringAsync().Result;
-                        result.Status = response.StatusCode;
+                        var response = await _httpClient.PostAsync(req.Url, hContent);
+                        result.IsSuccess = response.IsSuccessStatusCode;
                         if (response.IsSuccessStatusCode == false)
                         {
-                            result.Body = response.ReasonPhrase;
+                            result.StatusText = response.ReasonPhrase;
                         }
+                        else
+                        {
+                            result.Content = await response.Content.ReadAsStringAsync();
+                        }
+                        result.Status = response.StatusCode;
+                        result.RequestMessage = response.RequestMessage.ToString();
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
-                        Console.WriteLine("Request timeout \nCannot Connect to the server \nTry Again!!!", "Connect API Buddy");
+                        result.StatusText = ex.Message;
+                        return result;
                     }
+
                 }
-                else if (req.Method.Equals("DELETE"))
+                else if (req.BodyTabSelectedIndex == 1)
                 {
+                    var form = new MultipartFormDataContent();
+                    Dictionary<string, string> parameters = new Dictionary<string, string>();
+                    req.FormContent.ToList().ForEach(s =>
+                    {
+                        parameters.Add(s.Key, s.Value);
+                    });
+                    HttpContent FormItems = new FormUrlEncodedContent(parameters);
+                    form.Add(FormItems);
+
                     try
                     {
-                        var response = client.DeleteAsync(req.Url).Result;
-                        result.Body = response.Content.ReadAsStringAsync().Result;
-                        result.Status = response.StatusCode;
+                        var response = await _httpClient.PostAsync(req.Url, form);
+                        result.IsSuccess = response.IsSuccessStatusCode;
                         if (response.IsSuccessStatusCode == false)
                         {
-                            result.Body = response.ReasonPhrase;
+                            result.StatusText = response.ReasonPhrase;
                         }
+                        else
+                        {
+                            result.Content = await response.Content.ReadAsStringAsync();
+                        }
+                        result.Status = response.StatusCode;
+                        result.RequestMessage = response.RequestMessage.ToString();
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
-                        Console.WriteLine("Request timeout \nCannot Connect to the server \nTry Again!!!", "Connect API Buddy");
+                        result.StatusText = ex.Message;
+                        return result;
                     }
+
                 }
-                else
+                }
+            else if (req.Method.Equals("UPDATE"))
+            {
+                var hContent = new StringContent(req.PayloadBody);
+                hContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                try
                 {
-                    result.Body = "Method Incorrect";
+                    var response = await _httpClient.PutAsync(req.Url, hContent);
+                    result.IsSuccess = response.IsSuccessStatusCode;
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        result.StatusText = response.ReasonPhrase;
+                    }
+                    else
+                    {
+                        result.Content = await response.Content.ReadAsStringAsync();
+                    }
+                    result.Status = response.StatusCode;
+                    result.RequestMessage = response.RequestMessage.ToString();
+                }
+                catch (Exception ex)
+                {
+                    result.StatusText = ex.Message;
+                    return result;
                 }
             }
-
+            else if (req.Method.Equals("DELETE"))
+            {
+                try
+                {
+                    var response = await _httpClient.DeleteAsync(req.Url);
+                    result.IsSuccess = response.IsSuccessStatusCode;
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        result.StatusText = response.ReasonPhrase;
+                    }
+                    else
+                    {
+                        result.Content = await response.Content.ReadAsStringAsync();
+                    }
+                    result.Status = response.StatusCode;
+                    result.RequestMessage = response.RequestMessage.ToString();
+                }
+                catch (Exception ex)
+                {
+                    result.StatusText = ex.Message;
+                    return result;
+                }
+            }
+            else
+            {
+                result.Content = "Method Incorrect";
+            }
 
             return result;
-
         }
+
     }
 }
