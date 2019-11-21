256a3a0b7a9a8bd198b8fbd1d0c3c311c454cc56
KushanH <kushan989@gmail.com>
2019-11-21 16:49:28 +0530

    change prod backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
 
   apiUrls: {
-    backend_url: 'https://connectapibuddy-backend-prod.azurewebsites.net/api'
+    backend_url: 'https://connectapibuddy-backend.azurewebsites.net/api'
   }
 };
